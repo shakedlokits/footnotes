0fa0ce196dd2b75b858f97a9bdd825b078a1309d
Shaked Lokits <shaked.lokits@gmail.com>
2022-03-11 18:35:35 +0200

    Footnotes: Now using manual formatter.

--- node-external-diff.js
+++ node-external-diff.js
@@ -1,8 +1,11 @@
 import shell from "shelljs";
 import { createLogger, transports, format } from "winston";
+import parseDiff from 'parse-diff';
+import {readFileSync} from 'fs';
+import DiffMatchPatch from 'diff-match-patch';
 
 const logger = createLogger({
-  transports: [new transports.File({ filename: ".external-diff.log" })],
+  transports: [new transports.File({ filename: ".footnotes.log" })],
   format: format.combine(
     format.timestamp({ format: "YYYY-MM-dd HH:mm:ss,SSS" }),
     format.metadata({ fillExcept: ["message", "level", "timestamp", "label"] }),
@@ -23,7 +26,7 @@ const getParameters = () => {
   return { path, oldFile, oldHex, oldMode, newFile, newHex, newMode };
 };
 
-const getNativeFileDiff = (filePath) => {
+const getOriginalPatch = (filePath) => {
   const { stdout, stderr, code } = shell.exec(
     `git diff --no-ext-diff --exit-code -- ${filePath}`,
     { silent: true }
@@ -32,10 +35,36 @@ const getNativeFileDiff = (filePath) => {
   return { diff: stdout, error: stderr, fileCount: code };
 };
 
+function formatGitPatch(structuredPatch) {
+  const patchLines = [];
+
+  patchLines.push(`diff --git a/${structuredPatch.from} b/${structuredPatch.to}`);
+  patchLines.push(`index ${structuredPatch.index.join(' ')}`);
+
+  patchLines.push(`--- a/${structuredPatch.from}`);
+  patchLines.push(`--- b/${structuredPatch.to}`);
+
+  structuredPatch.chunks.forEach(chunk => {
+    patchLines.push(chunk.content);
+
+    chunk.changes.forEach(change => {
+      patchLines.push(change.content);
+    })
+  })
+
+  return patchLines.join('\n') + '\n';
+}
+
 const parameters = getParameters();
 logger.info("Running diff with parameters", parameters);
 
-const nativeResponse = getNativeFileDiff(parameters.path);
-logger.info("Received diff response", nativeResponse);
+const originalPatch = getOriginalPatch(parameters.path);
+logger.info("Received original patch", originalPatch);
 
-process.stdout.write(nativeResponse.diff);
+const [structuredPatch] = parseDiff(originalPatch.diff);
+logger.info("Parsed patch", structuredPatch);
+
+const patch = formatGitPatch(structuredPatch);
+logger.info("Reformatted parsed patch", patch);
+
+process.stdout.write(patch);
